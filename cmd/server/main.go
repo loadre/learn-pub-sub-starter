8ffd3e64c1e4e70601398e7953915bd6eb6c63a0
R Loader mkII <30044659+loadre@users.noreply.github.com>
2024-04-30 23:34:01 -0400

    trying to enable pubsub

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	pubsub "github.com/loadre/learn-pub-sub-starter/internal/pubsub"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	url := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	defer conn.Close()
+
+	connCh, err := conn.Channel()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Connection successful.")
+
+	// wait for ctrl+c
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt)
+	<-sigs
 }
